bc120fd3f41da7075c2c1691e66c4aa212f82a47
MalexLucero <40051264+MalexLucero@users.noreply.github.com>
2020-06-02 16:10:16 -0700

    Implemented Language Store
    Simplified APP by handing off state tracking to LanguageStore

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,24 @@
 import React, { Component } from "react";
 
-import LanguageContect from "../context/LanguageContext";
+import LanguageSelector from "../components/LanguageSelector";
+import { LanguageStore } from "../context/LanguageContext";
 import ColorContect from "../context/ColorContext";
 import UserCreate from "./UserCreate";
 
 class App extends Component {
-  state = {
-    language: "english",
-  };
-
-  onLanguageChange = (language) => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a Language:
-          <i
-            className="us flag"
-            onClick={() => this.onLanguageChange("english")}
-          />
-          <i
-            className="nl flag"
-            onClick={() => this.onLanguageChange("dutch")}
-          />
-        </div>
-        <ColorContect.Provider value="primary">
-          <LanguageContect.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContect.Provider value="primary">
             <UserCreate />
-          </LanguageContect.Provider>
-        </ColorContect.Provider>
+          </ColorContect.Provider>
+        </LanguageStore>
       </div>
     );
   }
 }
 
 export default App;
+
